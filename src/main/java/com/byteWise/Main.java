675dcbabb0ba7d03560fa73e28ad4524c299b2a5
dizzydroid <shehabmahmoud2003@gmail.com>
2024-05-04 21:34:28 +0300

    modified duplicate main method
    (one of our devs always ran the wrong one)

--- src/main/java/com/byteWise/Main.java
+++ src/main/java/com/byteWise/Main.java
@@ -10,18 +10,19 @@ import src.main.java.com.byteWise.courses.TextCourse;
 public class Main {
   
         public static void main(String[] args) {
-            List<Course> courses = new ArrayList<>();
-            Course textCourse = new TextCourse("1", "Aext Course", "Learn about text courses", "text",
-                    "https://www.textcourse.com");
+            System.out.println("\nYou ran the wrong main.");
+            // List<Course> courses = new ArrayList<>();
+            // Course textCourse = new TextCourse("1", "Aext Course", "Learn about text courses", "text",
+            //         "https://www.textcourse.com");
          
-            courses.add(textCourse);
+            // courses.add(textCourse);
          
-            courses.add(textCourse);
-            courses.add(textCourse);
-            Collections.sort(courses);
-            for (Course course : courses) {
-                course.getCourseTitle();
-            }
+            // courses.add(textCourse);
+            // courses.add(textCourse);
+            // Collections.sort(courses);
+            // for (Course course : courses) {
+            //     course.getCourseTitle();
+            // }
         }
     }
 
